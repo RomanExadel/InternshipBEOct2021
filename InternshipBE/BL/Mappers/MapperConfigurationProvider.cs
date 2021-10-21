349d2945fb28f375a852d15f1c1eb83f2086e8c9
ESinicin <catpuff13337@gmail.com>
2021-10-21 18:11:20 +0300

    Oops... I forgot to add to commit file and fixed it :)

---
+++ InternshipBE/BL/Mappers/MapperConfigurationProvider.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BL.Mappers
+{
+    public static class MapperConfigurationProvider
+    {
+        public static MapperConfigurationExpression Expression { get; }
+
+        static MapperConfigurationProvider()
+        {
+            Expression = new MapperConfigurationExpression();
+
+            Expression.AddMaps(Assembly.GetExecutingAssembly());
+        }
+
+        public static MapperConfiguration GetConfig()
+        {
+            var configuration = new MapperConfiguration(Expression);
+            
+            configuration.AssertConfigurationIsValid();
+
+            return configuration;
+        }
+
+        public static IServiceCollection AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddSingleton(GetConfig().CreateMapper());
+
+            return services;
+        }
+    }
+}
