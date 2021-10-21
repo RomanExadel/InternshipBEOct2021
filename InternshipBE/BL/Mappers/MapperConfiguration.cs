12f1a3fde8dd9bcb587d87ed8266a4697a411cd0
ESinicin <catpuff13337@gmail.com>
2021-10-21 12:57:09 +0300

    Installed AuntoMapper package and implemented static MapperConfiguration in BL.Mappers

---
+++ InternshipBE/BL/Mappers/MapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BL.Mappers
+{
+    public static class MapperConfiguration
+    {
+        public static AutoMapper.MapperConfiguration GetConfig(params Profile[] profiles)
+        {
+            return new AutoMapper.MapperConfiguration(mc => Array.ForEach(profiles, profile => mc.AddProfile(profile)));
+        }
+
+        public static IServiceCollection AddAutoMapper(this IServiceCollection services, IMapper mapper)
+        {
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+    }
+}
