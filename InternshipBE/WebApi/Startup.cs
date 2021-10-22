5b63508a870b9d7500a9f046dfd328ec7728144b
ESinicin <catpuff13337@gmail.com>
2021-10-22 18:21:38 +0300

    Added first implementation of exception handling middleware

--- InternshipBE/WebApi/Startup.cs
+++ InternshipBE/WebApi/Startup.cs
@@ -98,6 +98,8 @@ namespace WebApi
 				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
 			}
 
+			app.ConfigureExceptionHandler();
+
 			//app.UseHttpsRedirection();
 
 			app.UseRouting();
