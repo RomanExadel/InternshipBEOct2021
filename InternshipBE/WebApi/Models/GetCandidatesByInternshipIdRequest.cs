217574398359d79f7f791f5b14e61c90c315121c
ESinicin <catpuff13337@gmail.com>
2021-10-27 21:03:07 +0300

    Added spaces between properties in GetCandidatesByInternshipIdRequest

--- InternshipBE/WebApi/Models/GetCandidatesByInternshipIdRequest.cs
+++ InternshipBE/WebApi/Models/GetCandidatesByInternshipIdRequest.cs
@@ -3,7 +3,9 @@
     public class GetCandidatesByInternshipIdRequest
     {
         public int InternshipId { get; set; }
+
         public int ItemsCount { get; set; }
+
         public int PageNumber { get; set; }
     }
 }
