eb58023dabb0f677c161e3cbdc45436ac28998f7
Nikityanov <dnikityanov@gmail.com>
2021-10-28 21:42:45 +0300

    Change ICollection to List and Entities to DTO. Make InternshipStackDTO and UserInternshipDTO

---
+++ InternshipBE/BL/DTOs/InternshipStackDTO.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTOs
+{
+    public class InternshipStackDTO
+    {
+        public int Id { get; set; }
+
+        public int InternshipId { get; set; }
+
+        public StackType TechnologyStackType { get; set; }
+
+        public InternshipDTO Internship { get; set; }
+    }
+}
