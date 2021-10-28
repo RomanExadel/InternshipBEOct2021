af95cfe2241a696135657b94a0edb9b5815ede9f
Nikityanov <dnikityanov@gmail.com>
2021-10-28 22:07:56 +0300

    Implement Mapper profiles for UserInternship and InternshipStack entities and DTO's

---
+++ InternshipBE/BL/Mapping/Profiles/UserInternshipProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BL.DTOs;
+using DAL.Entities;
+
+namespace BL.Mapping.Profiles
+{
+    public class UserInternshipProfile: Profile
+    {
+        public UserInternshipProfile()
+        {
+            CreateMap<UserInternship, UserInternshipDTO>().ReverseMap();
+        }
+    }
+}
