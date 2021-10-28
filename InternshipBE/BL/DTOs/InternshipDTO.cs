6b6dd069cab524dfdb496c6c912638a6e12c802b
Nikityanov <dnikityanov@gmail.com>
2021-10-28 20:43:57 +0300

    Implement Internship service and controller. Implement API for Create, Update, Get one and Get all internships.

---
+++ InternshipBE/BL/DTOs/InternshipDTO.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Entities;
+using Shared.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace BL.DTOs
+{
+    public class InternshipDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Requirements { get; set; }
+
+        public int MaxCandidateCount { get; set; }
+
+        public DateTime RegistrationStartDate { get; set; }
+
+        public DateTime RegistrationFinishDate { get; set; }
+
+        public LanguageType LanguageType { get; set; }
+
+        public ICollection<Candidate> Candidate { get; set; }
+
+        public ICollection<InternshipStack> InternshipStack { get; set; }
+
+        public ICollection<UserInternship> UserInternship { get; set; }
+    }
+}
---
+++ InternshipBE/BL/Interfaces/IInternshipService.cs
@@ -0,0 +1,17 @@
+﻿using BL.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BL.Interfaces
+{
+    public interface IInternshipService
+    {
+        Task<InternshipDTO> GetInternshipAsync(int id);
+
+        Task<InternshipDTO> CreateInternshipAsync(InternshipDTO newInternship);
+
+        Task<InternshipDTO> UpdateInternshipAsync(InternshipDTO newInternship);
+
+        Task<List<InternshipDTO>> GetAllInternshipAsync();
+    }
+}
