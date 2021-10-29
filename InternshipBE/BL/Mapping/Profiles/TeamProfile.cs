267d647679e4c6b95817455774c6e346104f233e
ESinicin <catpuff13337@gmail.com>
2021-10-29 18:01:21 +0300

    Added necessary profiles and removed useless properties from UserDTO

---
+++ InternshipBE/BL/Mapping/Profiles/TeamProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BL.DTOs;
+using DAL.Entities;
+
+namespace BL.Mapping.Profiles
+{
+    public class TeamProfile : Profile
+    {
+        public TeamProfile()
+        {
+            CreateMap<Team, TeamDTO>().ReverseMap();
+        }
+    }
+}
