136e5d5705d11d36bea230b5b7d6e842dcc4d1d1
ESinicin <catpuff13337@gmail.com>
2021-10-29 18:01:21 +0300

    Added necessary profiles and removed useless properties from UserDTO

---
+++ InternshipBE/BL/DTOs/InternshipStackDTO.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Enums;
+
+namespace BL.DTOs
+{
+    public class InternshipStackDTO
+    {
+        public int Id { get; set; }
+
+        public int InternshipId { get; set; }
+
+        public StackType TechnologyStackType { get; set; }
+
+        public InternshipDTO Internship { get; set; }
+    }
+}
