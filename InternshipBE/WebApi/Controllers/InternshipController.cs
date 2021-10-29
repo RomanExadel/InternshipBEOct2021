d8ea7c96e35b9fa009ca615884f474b1ca5294bb
ESinicin <catpuff13337@gmail.com>
2021-10-29 18:48:34 +0300

    Margins are fixed

--- InternshipBE/WebApi/Controllers/InternshipController.cs
+++ InternshipBE/WebApi/Controllers/InternshipController.cs
@@ -1,8 +1,5 @@
-﻿using AutoMapper;
-using BL.DTOs;
+﻿using BL.DTOs;
 using BL.Interfaces;
-using BL.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using WebApi.Models;
@@ -39,7 +36,7 @@ namespace WebApi.Controllers
         [HttpPost("CreateInternship")]
         public async Task<IActionResult> CreateInternship ([FromBody] InternshipDTO internship)
         {
-          return Ok(await _service.CreateInternshipAsync(internship));
+            return Ok(await _service.CreateInternshipAsync(internship));
         }
 
         [HttpPut("UpdateInternship")]
