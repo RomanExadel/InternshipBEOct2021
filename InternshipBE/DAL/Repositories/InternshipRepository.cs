877990b0f8fd7136a4d03487f15107a43a37a442
Nikityanov <dnikityanov@gmail.com>
2021-10-29 14:40:01 +0300

    Changes in InternshipRepository

--- InternshipBE/DAL/Repositories/InternshipRepository.cs
+++ InternshipBE/DAL/Repositories/InternshipRepository.cs
@@ -2,9 +2,13 @@
 using DAL.Entities;
 using DAL.Interfaces;
 <<<<<<< HEAD
+<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
 =======
 >>>>>>> 955ab32 (Delete Useless DTO, Fixed Problems)
+=======
+using Microsoft.EntityFrameworkCore;
+>>>>>>> 0915da6 (Changes in InternshipRepository)
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,11 +24,15 @@ namespace DAL.Repositories
         public async Task<List<Internship>> GetAllInternshipsPartialAsync(int itemsCount, int pageNumber)
         {
 <<<<<<< HEAD
+<<<<<<< HEAD
             return await _context.Internships.Skip(itemsCount * pageNumber).Take(itemsCount).ToListAsync();
 =======
             var internships = _context.Internships.ToList().Skip(itemsCount * pageNumber).Take(itemsCount).ToList();
             return await Task.FromResult(internships);
 >>>>>>> 955ab32 (Delete Useless DTO, Fixed Problems)
+=======
+            return await _context.Internships.Skip(itemsCount * pageNumber).Take(itemsCount).ToListAsync();
+>>>>>>> 0915da6 (Changes in InternshipRepository)
         }
     }
 }
