357650b28fe1614a64ac48ea8feff885691faadc
Xodoc <maks.myshko.99@mail.ru>
2021-11-01 19:29:09 +0300

    Added user info controller

--- InternshipBE/BL/DTOs/UserDTO.cs
+++ InternshipBE/BL/DTOs/UserDTO.cs
@@ -5,16 +5,18 @@ namespace BL.DTOs
 {
     public class UserDTO
 	{
-        public int Id { get; set; }
-
-        public string UserName { get; set; }
-
-        public string Email { get; set; }
-
-        public RoleType RoleType { get; set; }
+        public string Id { get; set; }
 
         public string Position { get; set; }
 
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
         public DateTime BestContactTime { get; set; }
+
+        public string UserName { get; set; }
+
+        public RoleType RoleType { get; set; }
     }
 }
--- InternshipBE/BL/Services/UserService.cs
+++ InternshipBE/BL/Services/UserService.cs
@@ -1,5 +1,8 @@
-﻿using BL.Interfaces;
+﻿using AutoMapper;
+using BL.DTOs;
+using BL.Interfaces;
 using DAL.Entities;
+using DAL.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -12,68 +15,84 @@ using System.Threading.Tasks;
 
 namespace BL.Services
 {
-	public class UserService : IUserService
-	{
-		private readonly UserManager<User> _userManager;
-		private readonly RoleManager<IdentityRole> _roleManager;
-		private readonly IConfiguration _configuration;
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
 
-		public UserService(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
-		{
-			_userManager = userManager;
-			_configuration = configuration;
-			_roleManager = roleManager;
-		}
+        public UserService(UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration, IMapper mapper)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
 
+        public async Task<string> AuthenticateAsync(string login, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(login);
 		public async Task<string> AuthenticateAsync(string email, string password)
 		{
 			var user = await _userManager.FindByEmailAsync(email);
 
-			if (user != null && await _userManager.CheckPasswordAsync(user, password))
-			{
-				await EnsureCreateRoleAsync(user);
+            if (user != null && await _userManager.CheckPasswordAsync(user, password))
+            {
+                await EnsureCreateRoleAsync(user);
 
-				var roles = await _userManager.GetRolesAsync(user);
-				var authClaims = new List<Claim>
-				{
-					new Claim(ClaimTypes.Name, user.UserName),
-					new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-				};
+                var roles = await _userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
 
-				foreach(var role in roles)
-				{
-					authClaims.Add(new Claim(ClaimTypes.Role, role));
-				}
+                foreach (var role in roles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
 
-				var authSigninKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                var authSigninKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
 
-				var token = new JwtSecurityToken(
-				_configuration["Jwt:Issuer"],
-				_configuration["Jwt:Audience"],
-				notBefore: DateTime.UtcNow,
-				claims: authClaims,
-				expires: DateTime.UtcNow.Add(TimeSpan.FromDays(int.Parse(_configuration["Jwt:LifeTimeInDays"]))), 
-				signingCredentials: new SigningCredentials(authSigninKey, SecurityAlgorithms.HmacSha256));
+                var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                notBefore: DateTime.UtcNow,
+                claims: authClaims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromDays(int.Parse(_configuration["Jwt:LifeTimeInDays"]))),
+                signingCredentials: new SigningCredentials(authSigninKey, SecurityAlgorithms.HmacSha256));
 
-				var encodedJwt = new JwtSecurityTokenHandler().WriteToken(token);
+                var encodedJwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-				return encodedJwt;
-			}
+                return encodedJwt;
+            }
 
-			return string.Empty;
-		}
+            return string.Empty;
+        }
 
-		private async Task EnsureCreateRoleAsync(User user) 
-		{
-			if (!await _roleManager.RoleExistsAsync(user.RoleType.ToString()))
-			{
-				await _roleManager.CreateAsync(new IdentityRole(user.RoleType.ToString()));
-			}
+        private async Task EnsureCreateRoleAsync(User user)
+        {
+            if (!await _roleManager.RoleExistsAsync(user.RoleType.ToString()))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(user.RoleType.ToString()));
+            }
 
-			if (await _roleManager.RoleExistsAsync(user.RoleType.ToString()))
-			{
-				await _userManager.AddToRoleAsync(user, user.RoleType.ToString());
-			}
-		}
-	}
+            if (await _roleManager.RoleExistsAsync(user.RoleType.ToString()))
+            {
+                await _userManager.AddToRoleAsync(user, user.RoleType.ToString());
+            }
+        }
+
+        public async Task<UserDTO> GetUserInfoAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                return _mapper.Map<UserDTO>(user);
+            }
+            return null;
+        }
+    }
 }
