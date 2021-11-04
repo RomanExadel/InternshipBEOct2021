f1c4fe92e346731072f5584a4ade6562f9f9656b
ESinicin <catpuff13337@gmail.com>
2021-11-04 16:26:00 +0300

    Implemented InternshipStackRepository

---
+++ InternshipBE/DAL/Interfaces/IInternshipStackRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IInternshipStackRepository
+    {
+        Task<List<InternshipStack>> GetInternshipStacksByInternshipIdAsync(int id);
+    }
+}
---
+++ InternshipBE/DAL/Repositories/InternshipStackRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Database;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class InternshipStackRepository : GenericRepository<InternshipStack>, IInternshipStackRepository
+    {
+        public InternshipStackRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Task<List<InternshipStack>> GetInternshipStacksByInternshipIdAsync(int id)
+        {
+            return _context.InternshipStacks.Where(x => x.InternshipId == id).ToListAsync();
+        }
+    }
+}
