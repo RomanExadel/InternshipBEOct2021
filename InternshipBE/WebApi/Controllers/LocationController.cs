47d937078e1e3feaa69419eda28c276d46761a88
Xodoc <maks.myshko.99@mail.ru>
2021-11-11 16:03:46 +0300

    Implement Location controller

---
+++ InternshipBE/WebApi/Controllers/LocationController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        // GET: api/<LocationController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        // GET api/<LocationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LocationController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LocationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LocationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
