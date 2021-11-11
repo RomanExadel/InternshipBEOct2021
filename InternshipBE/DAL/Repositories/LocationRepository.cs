2a7cd5f1ffc52c2547d976a3df78228f917c1cf6
Xodoc <maks.myshko.99@mail.ru>
2021-11-11 16:03:46 +0300

    Implement Location controller

---
+++ InternshipBE/DAL/Repositories/LocationRepository.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Database;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class LocationRepository : GenericRepository<Country>, ILocationRepository
+    {
+        public LocationRepository(ApplicationDbContext context): base(context)
+        {            
+        }
+
+        public async Task<List<Country>> GetLocationsAsync()
+        {
+            var locations = await _context.Countries.ToListAsync();
+            
+            
+
+            return null;
+        }
+    }
+}
