8ac2b2c2ab0abcebce231c0a92b61c729403fdb4
ESinicin <catpuff13337@gmail.com>
2021-11-12 16:48:15 +0300

    InternshipStacks were fixed

--- InternshipBE/BL/Services/InternshipService.cs
+++ InternshipBE/BL/Services/InternshipService.cs
@@ -3,7 +3,9 @@ using BL.DTOs;
 using BL.Interfaces;
 using DAL.Entities;
 using DAL.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BL.Services
