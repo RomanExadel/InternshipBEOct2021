b7405bc4d0cbee94fcb62563d732adeb7387e947
ESinicin <catpuff13337@gmail.com>
2021-11-12 15:43:46 +0300

    Returned type was changed

--- InternshipBE/BL/Interfaces/ILocationService.cs
+++ InternshipBE/BL/Interfaces/ILocationService.cs
@@ -8,6 +8,6 @@ namespace BL.Interfaces
     {
         Task<List<CountryDTO>> GetLocationsAsync();
 
-        Task<CountryDTO> CreateLocationAsync(CountryDTO NameLocation);
+        Task<GetCountryDTO> CreateLocationAsync(CountryDTO NameLocation);
     }
 }
