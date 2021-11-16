8c9faaebeb56b3df292d730233af988c49bfe61a
ESinicin <catpuff13337@gmail.com>
2021-11-16 15:34:41 +0300

    Evaluation service was fixed

--- InternshipBE/DAL/Migrations/20211112144337_SeedData.cs
+++ InternshipBE/DAL/Migrations/20211112144337_SeedData.cs
@@ -44,12 +44,12 @@ namespace DAL.Migrations
                 columns: new[] { "Id", "TechnologyStackType" },
                 values: new object[,]
                 {
-                    { 1, 1 },
-                    { 2, 2 },
-                    { 3, 3 },
-                    { 4, 4 },
-                    { 5, 5 },
-                    { 6, 6 }
+                    { 1, 0 },
+                    { 2, 1 },
+                    { 3, 2 },
+                    { 4, 3 },
+                    { 5, 4 },
+                    { 6, 5 }
                 });
 
             migrationBuilder.InsertData(
