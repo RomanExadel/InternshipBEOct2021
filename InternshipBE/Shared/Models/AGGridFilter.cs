0528b97434cd3dcd03f0952d6c5f4752b6aeac40
vldmrklnk <smile.traser@gmail.com>
2021-11-17 23:07:42 +0300

    AGGridFilter realisation

--- InternshipBE/Shared/Models/AGGridFilter.cs
+++ InternshipBE/Shared/Models/AGGridFilter.cs
@@ -2,5 +2,14 @@
 {
 	public class AGGridFilter
 	{
+		public int Skip { get; set; }
+
+		public int Take { get; set; }
+
+		public string SearchText { get; set; }
+
+		public string SortBy { get; set; }
+
+		public bool IsDesc { get; set; }
 	}
 }
