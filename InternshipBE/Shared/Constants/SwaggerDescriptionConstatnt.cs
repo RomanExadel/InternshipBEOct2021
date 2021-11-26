310bc813429101e40581115172c9331cd92b6e19
Xodoc <maks.myshko.99@mail.ru>
2021-11-26 20:42:38 +0300

    Corrected SwaggerUI

--- InternshipBE/Shared/Constants/SwaggerDescriptionConstatnt.cs
+++ InternshipBE/Shared/Constants/SwaggerDescriptionConstatnt.cs
@@ -2,48 +2,48 @@
 {
 	public static class SwaggerDescriptionConstatnt
 	{
-		public const string SwaggerDescription = "Тут я могу писать важную инфу для вас.\n" +
-			"email: admntest.team5@gmail.com\n" +
-			"email: hrlily.team5@gmail.com\n" +
-			"email: interviewer.team5@gmail.com\n" +
-			"email: managertest.team5@gmail.com\n" +
-			"email: mentortest.team5@gmail.com\n" +
-			"Пароль у всех: qwErtY2021\n" +
-			"EnglishLevelType:\n" +
-			"0 - A1\n" +
-			"1 - A2\n" +
-			"3 - B1\n" +
-			"4 - B2\n" +
-			"5 - C1\n" +
-			"6 - C2\n" +
-			"LanguageType:\n" +
-			"0 - English\n" +
-			"1 - Russian\n" +
-			"2 - German\n" +
-			"3 - French\n" +
-			"4 - Indian\n" +
-			"5 - Chinese\n" +
-			"RoleType:\n" +
-			"0 - Mentor\n" +
-			"1 - Hr\n" +
-			"2 - Interviewer\n" +
-			"3 - Manager\n" +
-			"4 - Admin\n" +
-			"StackType:\n" +
-			"0 - FrontEnd\n" +
-			"1 - BackEnd\n" +
-			"2 - FullStack\n" +
-			"3 - BusinessAnalysis\n" +
-			"4 - DevOps\n" +
-			"5 - Testing\n" +
-			"StatusType:\n" +
-			"0 - New\n" +
-			"1 - HR\n" +
-			"2 - TestTask\n" +
-			"3 - Interview\n" +
-			"4 - Pending\n" +
-			"5 - Accepted\n" +
-			"6 - Questionable\n" +
-			"7 - Declined\n";
+		public const string SwaggerDescription = "Тут я могу писать важную инфу для вас." +
+			"</br>**email:** admntest.team5@gmail.com" +
+			"</br>**email:** hrlily.team5@gmail.com" +
+			"</br>**email:** interviewer.team5@gmail.com" +
+			"</br>**email:** managertest.team5@gmail.com" +
+			"</br>**email:** mentortest.team5@gmail.com" +
+			"</br>**Пароль у всех:** qwErtY2021" +
+			"</br>**EnglishLevelType:** " +
+			"0 - A1; " +
+			"1 - A2; " +
+			"3 - B1; " +
+			"4 - B2; " +
+			"5 - C1; " +
+			"6 - C2." +
+			"</br>**LanguageType:** " +
+			"0 - English; " +
+			"1 - Russian; " +
+			"2 - German; " +
+			"3 - French; " +
+			"4 - Indian; " +
+			"5 - Chinese." +
+			"</br>**RoleType:** " +
+			"0 - Mentor; " +
+			"1 - Hr; " +
+			"2 - Interviewer; " +
+			"3 - Manager; " +
+			"4 - Admin." +
+			"</br>**StackType:** " +
+			"0 - FrontEnd; " +
+			"1 - BackEnd; " +
+			"2 - FullStack; " +
+			"3 - BusinessAnalysis; " +
+			"4 - DevOps; " +
+			"5 - Testing. " +
+			"</br>**StatusType:** " +
+			"0 - New; " +
+			"1 - HR; " +
+			"2 - TestTask; " +
+			"3 - Interview; " +
+			"4 - Pending; " +
+			"5 - Accepted; " +
+			"6 - Questionable; " +
+			"7 - Declined.";
 	}
 }
