2cef07fefd0b2c6153fbd1d575c74dbb73e8d71a
Xodoc <maks.myshko.99@mail.ru>
2021-11-27 00:25:33 +0300

    Remove unnecessary namespace

--- InternshipBE/DAL/Repositories/InternshipRepository.cs
+++ InternshipBE/DAL/Repositories/InternshipRepository.cs
@@ -3,7 +3,6 @@ using DAL.Entities;
 using DAL.Entities.Filtering;
 using DAL.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -35,15 +34,15 @@ namespace DAL.Repositories
             return internship;
         }
 
-        public override async Task<Internship> UpdateAsync(Internship newInternship) 
+        public override async Task<Internship> UpdateAsync(Internship newInternship)
         {
             var internship = await _context.Internships
                 .Include(x => x.Teams)
-                .Include(x =>x.Users)
-                .Include(x =>x.Candidates)
-                .Include(x =>x.Countries)
-                .Include(x=>x.InternshipStacks)
-                .FirstOrDefaultAsync(x =>x.Id == newInternship.Id);
+                .Include(x => x.Users)
+                .Include(x => x.Candidates)
+                .Include(x => x.Countries)
+                .Include(x => x.InternshipStacks)
+                .FirstOrDefaultAsync(x => x.Id == newInternship.Id);
 
             _validator.ValidateIfEntityExist(internship);
 
@@ -56,18 +55,18 @@ namespace DAL.Repositories
             internship.RegistrationStartDate = newInternship.RegistrationStartDate;
             internship.Candidates = await _context.Candidates.Where(x => x.InternshipId == newInternship.Id).ToListAsync();
             internship.LanguageTypes = newInternship.LanguageTypes;
-            internship.InternshipStacks = await _context.InternshipStacks.Where(x =>x.InternshipId == newInternship.Id).ToListAsync();
+            internship.InternshipStacks = await _context.InternshipStacks.Where(x => x.InternshipId == newInternship.Id).ToListAsync();
             internship.Users = await _context.Users.Where(x => newInternship.Users.Contains(x)).ToListAsync();
             internship.Teams = await _context.Teams.Where(x => x.InternshipId == newInternship.Id).ToListAsync();
             internship.Countries = await _context.Countries.Where(x => newInternship.Countries.Contains(x)).ToListAsync();
             internship.InternshipStatusType = newInternship.InternshipStatusType;
-            internship.ImageLink = newInternship.ImageLink;            
+            internship.ImageLink = newInternship.ImageLink;
 
             _context.Internships.Update(internship);
 
             await _context.SaveChangesAsync();
-            
-            return newInternship;
+
+            return internship;
         }
 
         public async Task<List<Internship>> GetInternshipsAsync(int pageSize, int pageNumber, InternshipFilterModel filterBy)
@@ -93,7 +92,7 @@ namespace DAL.Repositories
 
         private List<Internship> GetFilteredInternshipsAsync(int pageSize, int pageNumber, InternshipFilterModel filterBy)
         {
-            var internships =  _context.Internships.AsQueryable();
+            var internships = _context.Internships.AsQueryable();
 
             if (filterBy.Locations != null)
                 foreach (var location in filterBy.Locations)
@@ -106,7 +105,7 @@ namespace DAL.Repositories
                     internships = internships.Where(i => i.LanguageTypes.Any(l => l.LanguageType == language));
                 }
             if (filterBy.InternshipStacks != null)
-                foreach(var stack in filterBy.InternshipStacks)
+                foreach (var stack in filterBy.InternshipStacks)
                 {
                     internships = internships.Where(i => i.InternshipStacks.Any(s => s.TechnologyStackType == stack));
                 }
@@ -115,10 +114,10 @@ namespace DAL.Repositories
                 {
                     internships = internships.Where(i => i.Users.Any(u => u.UserName.Contains(userName)));
                 }
-            if(filterBy.IntershipYear.HasValue)
+            if (filterBy.IntershipYear.HasValue)
                 internships = internships.Where(i => i.StartDate.Year == filterBy.IntershipYear);
 
-            return  internships.AsNoTracking()
+            return internships.AsNoTracking()
                                     .Include(x => x.InternshipStacks)
                                     .Include(x => x.Countries)
                                     .Include(x => x.Candidates)
