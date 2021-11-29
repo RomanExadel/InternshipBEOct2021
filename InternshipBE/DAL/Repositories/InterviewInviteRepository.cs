8c811a5093ea4820c6c05a897a009885875b4256
ESinicin <catpuff13337@gmail.com>
2021-11-29 13:38:33 +0300

    InteviewInviteRepository class was implemented

---
+++ InternshipBE/DAL/Repositories/InterviewInviteRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Database;
+using DAL.Entities;
+using DAL.Interfaces;
+
+namespace DAL.Repositories
+{
+    public class InterviewInviteRepository : GenericRepository<InterviewInvite>, IInterviewInviteRepository
+    {
+        public InterviewInviteRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
