73f96cff971a9b2df1cc4172d23883d9192bdcdb
ESinicin <catpuff13337@gmail.com>
2021-11-29 13:09:24 +0300

    Created IInterviewInvite interface

---
+++ InternshipBE/BL/Interfaces/IInterviewInviteService.cs
@@ -0,0 +1,6 @@
+﻿namespace BL.Interfaces
+{
+    public interface IInterviewInviteService
+    {
+    }
+}
--- InternshipBE/WebApi/Controllers/InterviewInviteController.cs
+++ InternshipBE/WebApi/Controllers/InterviewInviteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
