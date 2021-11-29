9f2c415a7d12c67ccb63c63ac1b93c45f474c281
ESinicin <catpuff13337@gmail.com>
2021-11-29 13:07:29 +0300

    Created interview invite controller

---
+++ InternshipBE/WebApi/Controllers/InterviewInviteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterviewInviteController : Controller
+    {
+        private readonly IInterviewInviteService _interviewInviteService;
+        
+        public InterviewInviteController(IInterviewInviteService interviewInviteService)
+        {
+            _interviewInviteService = interviewInviteService;
+        }
+    }
+}
