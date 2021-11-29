635e40f5794c50a5d70c8bca62b0fe94a267939c
ESinicin <catpuff13337@gmail.com>
2021-11-29 15:44:48 +0300

    Created InterviewInviteDTO validator

---
+++ InternshipBE/WebApi/Validators/InterviewInviteDTOValidator.cs
@@ -0,0 +1,15 @@
+﻿using BL.DTOs;
+using FluentValidation;
+
+namespace WebApi.Validators
+{
+    public class InterviewInviteDTOValidator : AbstractValidator<InterviewInviteDTO>
+    {
+        public InterviewInviteDTOValidator()
+        {
+            RuleFor(invite => invite.CandidateId).NotEmpty();
+            RuleFor(invite => invite.ContactDate).NotEmpty();
+            RuleFor(invite => invite.UserId).NotEmpty();
+        }
+    }
+}
