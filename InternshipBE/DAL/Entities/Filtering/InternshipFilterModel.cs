a4376b535785d1585d11032a467963a13222526c
ihhyy <ihhyman.ii@gmail.com>
2021-12-02 20:32:00 +0300

    update internship filter model

--- InternshipBE/DAL/Entities/Filtering/InternshipFilterModel.cs
+++ InternshipBE/DAL/Entities/Filtering/InternshipFilterModel.cs
@@ -2,17 +2,17 @@
 
 namespace DAL.Entities.Filtering
 {
-    public class InternshipFilterModel 
+    public class InternshipFilterModel<T>
     {
-        public List<string> Locations { get; set; }
+        public List<T> Locations { get; set; }
 
-        public List<string> LanguageTypes { get; set; }
+        public List<T> LanguageTypes { get; set; }
 
         public string InternshipStatusType { get; set; }
 
-        public List<string> InternshipStacks { get; set; }
+        public List<T> InternshipStacks { get; set; }
 
-        public List<string> AttachedUsers { get; set; }
+        public List<T> AttachedUsers { get; set; }
 
         public int IntershipYear { get; set; }
     }
