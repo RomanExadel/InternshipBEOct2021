3c2a93a94cca6e7bc883e9abffb79995ee8e87b4
ESinicin <catpuff13337@gmail.com>
2021-12-06 16:02:16 +0300

    Created CreateEvaluationAsync_WhenFeedbackIsCorrect_GettingNewEvaluation test

--- InternshipBE/Tests/Services/EvaluationServiceTest.cs
+++ InternshipBE/Tests/Services/EvaluationServiceTest.cs
@@ -34,30 +34,57 @@ namespace Tests.Services
         [Fact]
         public async void GetEvaluationsByFeedbackIdAsync_WhenFeedbackIsExist_GettingEvaluations()
         {
+            //Arrange
             var feedbackId = 1;
             var outputEvaluations = _mapper.Map<List<Evaluation>>(_evaluationDtos.Where(x => x.FeedbackId == feedbackId).ToList());
             var expectedEvaluationDtos = _evaluationDtos.Where(x => x.FeedbackId == feedbackId).ToList();
 
+            //Act
             _uowMock.Setup(x => x.Evaluations.GetEvaluationsByFeedbackIdAsync(It.IsAny<int>()))
                 .ReturnsAsync(outputEvaluations);
 
             var actualEvaluationDtos = await _evaluationService.GetEvaluationsByFeedbackIdAsync(feedbackId);
 
+            //Assert
             Assert.Equal(expectedEvaluationDtos, actualEvaluationDtos, new EvaluationDTOEqualityComparer());
         }
 
         [Fact]
         public async void GetEvaluationsByFeedbackIdAsync_WhenFeedbackIsNotExist_GettingEmptyList()
         {
+            //Arrange
             var feedbackId = -1;
             var outputEvaluations = _mapper.Map<List<Evaluation>>(_evaluationDtos.Where(x => x.FeedbackId == feedbackId).ToList());
             var expectedEvaluationDtos = new List<EvaluationDTO>();
 
+            //Act
             _uowMock.Setup(x => x.Evaluations.GetEvaluationsByFeedbackIdAsync(feedbackId))
                 .ReturnsAsync(outputEvaluations);
 
             var actualEvaluationDtos = await _evaluationService.GetEvaluationsByFeedbackIdAsync(feedbackId);
 
+            //Assert
+            Assert.Equal(expectedEvaluationDtos, actualEvaluationDtos, new EvaluationDTOEqualityComparer());
+        }
+        
+        [Fact]
+        public async void CreateEvaluationAsync_WhenFeedbackIsCorrect_GettingNewEvaluation()
+        {
+            //Arrange
+            var inputEvaluationDTO = new EvaluationDTO { Id = 0, FeedbackId = 1, SkillId = 1, Value = 1 };
+            var inputEvaluation = _mapper.Map<Evaluation>(inputEvaluationDTO);
+            var outputEvaluation = new Evaluation { Id = 1, FeedbackId = 1, SkillId = 1, Value = 1 };
+            var expectedEvaluationDtos = _mapper.Map<EvaluationDTO>(outputEvaluation);
+
+            //Act
+            _uowMock.Setup(x => x.Evaluations.CreateAsync(It.IsAny<Evaluation>()))
+                .ReturnsAsync(outputEvaluation);
+            _uowMock.Setup(x => x.Skills.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(null as Skill);
+
+            var actualEvaluationDtos = await _evaluationService.CreateEvaluationAsync(inputEvaluationDTO);
+
+            //Assert
             Assert.Equal(expectedEvaluationDtos, actualEvaluationDtos, new EvaluationDTOEqualityComparer());
         }
     }
