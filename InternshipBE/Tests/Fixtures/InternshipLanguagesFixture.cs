c6513ff99875f8c01588c967d9349cd3f459e3f5
Xodoc <maks.myshko.99@mail.ru>
2021-12-06 16:28:49 +0300

    Implement InternshipLanguages test

---
+++ InternshipBE/Tests/Fixtures/InternshipLanguagesFixture.cs
@@ -0,0 +1,49 @@
+﻿using DAL.Entities;
+using Shared.Enums;
+using System.Collections.Generic;
+
+namespace Tests.Fixtures
+{
+    public class InternshipLanguagesFixture
+    {
+        private readonly List<Internship> _internships;
+
+        public InternshipLanguagesFixture()
+        {
+            _internships = new List<Internship>
+            {
+                new Internship
+                {
+                    LanguageTypes = new List<InternshipLanguage>
+                    {
+                        new InternshipLanguage { Id = 1, LanguageType = InternshipLanguageType.English, InternshipId = 1, Internship = null },
+                        new InternshipLanguage { Id = 2, LanguageType = InternshipLanguageType.German, InternshipId = 1, Internship = null }
+                    }
+                },
+                new Internship
+                {
+                    LanguageTypes = new List<InternshipLanguage>
+                    {
+                        new InternshipLanguage { Id = 1, LanguageType = InternshipLanguageType.English, InternshipId = 1, Internship = null },
+                        new InternshipLanguage { Id = 3, LanguageType = InternshipLanguageType.French, InternshipId = 1, Internship = null }
+                    }
+                },
+                new Internship
+                {
+                    LanguageTypes = new List<InternshipLanguage>
+                    {
+                        new InternshipLanguage { Id = 4, LanguageType = InternshipLanguageType.Russian, InternshipId = 1, Internship = null },
+                        new InternshipLanguage { Id = 5, LanguageType = InternshipLanguageType.Russian, InternshipId = 2, Internship = null }
+                    }
+                }
+
+            };
+        }
+
+        public List<Internship> GetInternships() 
+        {
+            return _internships;
+        }
+
+    }
+}
---
+++ InternshipBE/Tests/Services/InternshiplanguagesServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using BL.EqualityComparers;
+using BL.Interfaces;
+using BL.Services;
+using DAL.Entities;
+using DAL.Interfaces;
+using Moq;
+using Tests.Fixtures;
+using Xunit;
+
+namespace Tests.Services
+{
+    public class InternshiplanguagesServiceTest
+    {
+        private readonly Mock<IUnitOfWork> _uowMock;
+        private readonly IInternshipLanguagesService _internshipLanguagesService;
+        private readonly InternshipLanguagesFixture _internshipLanguagesFixture;
+
+        public InternshiplanguagesServiceTest()
+        {
+            _uowMock = new Mock<IUnitOfWork>();
+            _internshipLanguagesService = new InternshipLanguagesService(_uowMock.Object);
+            _internshipLanguagesFixture = new InternshipLanguagesFixture();
+
+        }
+
+        [Fact]
+        public async void CreateOrDeleteLanguagesAsync_IfNewLanguagesCountMoreOrEquelOldLanguagesCount_CreateLanguages()
+        {
+            // arrange
+            var internshipLanguagesFixture = _internshipLanguagesFixture.GetInternships();
+            var newInternship = internshipLanguagesFixture[0]; 
+            var oldInternship = internshipLanguagesFixture[1];
+
+            Internship expected = internshipLanguagesFixture[0];
+
+            _uowMock.Setup(x => x.InternshipLanguages.CreateOrDeleteLanguagesAsync(It.IsAny<Internship>(), It.IsAny<Internship>()))
+                .ReturnsAsync(newInternship);
+
+            // act
+            var actual = await _internshipLanguagesService.CreateOrDeleteLanguagesAsync(oldInternship, newInternship);
+
+            // assert
+            Assert.Equal(expected, actual, new InternshipLanguagesEqualityComparer());
+        }
+    }
+}
