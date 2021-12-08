05c5b61f078bce1cb22198119174e46b8bac1109
ihhyy <ihhyman.ii@gmail.com>
2021-12-08 17:54:02 +0300

    implement locations test

---
+++ InternshipBE/BL/EqualityComparers/LocationDTOEqualityComparer.cs
@@ -0,0 +1,26 @@
+﻿using BL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BL.EqualityComparers
+{
+    public class LocationDTOEqualityComparer : IEqualityComparer<CountryDTO>
+    {
+        public bool Equals(CountryDTO x, CountryDTO y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+
+            return x.Id == y.Id && x.Name == y.Name;
+        }
+
+        public int GetHashCode([DisallowNull] CountryDTO obj)
+        {
+            return HashCode.Combine(obj.Id, obj.Name);
+        }
+
+    }
+}
---
+++ InternshipBE/Tests/Services/LocationServiceTest.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using DAL.Interfaces;
+using Moq;
+using Xunit;
+using System.Collections.Generic;
+using Tests.Fixtures;
+using BL.Interfaces;
+using BL.EqualityComparers;
+using BL.Services;
+using BL.Mapping.Profiles;
+using BL.DTOs;
+using System.Linq;
+using DAL.Entities;
+
+namespace Tests.Services
+{
+    public class LocationServiceTest
+    {
+        private readonly Mock<IUnitOfWork> _uowMock;
+        private readonly LocationFixture _locationFixture;
+        private readonly ILocationService _locationService;
+        private readonly IMapper _mapper;
+
+        public LocationServiceTest()
+        {
+            _locationFixture = new LocationFixture();
+            _uowMock = new Mock<IUnitOfWork>();
+            _mapper = new MapperConfiguration(cfg => cfg.AddProfile(new CountryProfile())).CreateMapper();
+            _locationService = new LocationService(_mapper, _uowMock.Object);
+        }
+
+        [Fact]
+        public async void GetAllLocations()
+        {
+            //Arrange
+            var locations = _locationFixture.GetCountries();
+            var expectedLocations = _mapper.Map<List<CountryDTO>>(locations);
+
+            //Act
+            _uowMock.Setup(x => x.Locations.GetAllAsync()).ReturnsAsync(locations);
+            var actual = await _locationService.GetLocationsAsync();
+
+            //Assert
+            Assert.Equal(expectedLocations, actual, new LocationDTOEqualityComparer());
+        }
+
+        [Fact]
+        public async void GetAllLocationsNames()
+        {
+            //Arrange
+            var locationsNames = _locationFixture.GetCountries().Select(x => x.Name).Distinct().ToList();
+            var expectedLocations = _mapper.Map<List<string>>(locationsNames);
+
+            //Act
+            _uowMock.Setup(x => x.Locations.GetLocationNamesAsync()).ReturnsAsync(locationsNames);
+            var actual = await _locationService.GetLocationNamesAsync();
+
+            //Assert
+            Assert.Equal(expectedLocations, actual);
+        }
+
+        [Fact]
+        public async void CreateLocation()
+        {
+            //Arrange
+            var inputLocation = new Country { Id = 0, Name = "Name" };
+            var outputLocation = new Country { Id = 5, Name = "Name" };
+            var inputLocationDTO = _mapper.Map<CountryDTO>(inputLocation);
+            var expectedLocationDTO = _mapper.Map<CountryDTO>(outputLocation);
+            _uowMock.Setup(x => x.Locations.CreateLocationAsync(It.IsAny<string>()))
+                .ReturnsAsync(outputLocation);
+
+            //Act
+            var actualLocationDTO = await _locationService.CreateLocationAsync(inputLocationDTO);
+
+            //Assert
+            Assert.Equal(expectedLocationDTO, actualLocationDTO, new LocationDTOEqualityComparer());
+        }
+    }
+}
