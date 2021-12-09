afb2ba7e9690eda17f0922d5e95db765d469d4bc
ESinicin <catpuff13337@gmail.com>
2021-12-09 17:33:02 +0300

    SkillService was injected to SkillServiceTest

---
+++ InternshipBE/Tests/IntegrationTests/Services/SkillServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using BL.Interfaces;
+using Xunit;
+
+namespace Tests.IntegrationTests.Services
+{
+    public class SkillServiceTest
+    {
+        ISkillService _skillService;
+
+        public SkillServiceTest(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+        [Fact]
+        public void K()
+        {
+            
+
+            Assert.NotNull(_skillService);
+        }
+    }
+}
---
+++ InternshipBE/Tests/Startup.cs
@@ -0,0 +1,34 @@
+﻿using BL.Interfaces;
+using BL.Mapping;
+using BL.Services;
+using DAL.Database;
+using DAL.Entities;
+using DAL.Interfaces;
+using DAL.Repositories;
+using DAL.Validators;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>();
+
+            services.AddScoped<IUnitOfWork, EFUnitOfWork>();
+
+            services.AddScoped<IValidator<Candidate>, CandidateValidator>();
+            services.AddScoped<IValidator<User>, UserValidator>();
+            services.AddScoped<IValidator<Feedback>, FeedbackValidator>();
+            services.AddScoped<IValidator<Internship>, InternshipValidator>();
+            services.AddScoped<IValidator<Evaluation>, EvaluationValidator>();
+            services.AddScoped<IValidator<InterviewInvite>, InterviewInviteValidator>();
+            services.AddScoped<IValidator<Team>, TeamValidator>();
+
+            services.AddScoped<ISkillService, SkillService>();
+
+            services.AddAutoMapper();
+        }
+    }
+}
